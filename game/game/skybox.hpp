fac582d0daa5e48b5f87439650143bd5aaf6f99d
naritta <narittan@gmail.com>
2021-08-04 19:40:30 +0900

    Add skybox

---
+++ game/game/skybox.hpp
@@ -0,0 +1,14 @@
+//
+//  skybox.hpp
+//  game
+//
+//  Created by Ritta Narita on 2021/08/04.
+//  Copyright © 2021 Ritta Narita. All rights reserved.
+//
+
+#ifndef skybox_hpp
+#define skybox_hpp
+
+#include <stdio.h>
+
+#endif /* skybox_hpp */
